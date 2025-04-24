62db74dd9d38f378aba0f9bb94f5458b3a7887ae
NicknineTheEagle <nik18.1995@gmail.com>
2025-04-25 01:24:31 +0300

    Initial code commit

---
+++ NFSCarbonDLCUnlocker/NFSCarbonDLCUnlocker.cpp
@@ -0,0 +1,73 @@
+#include <Windows.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <MinHook.h>
+
+std::vector<int> g_dlcList;
+
+int bStringHash( const char *a1 )
+{
+	const char *v1; // edx
+	unsigned __int8 v2; // cl
+	int result; // eax
+
+	v1 = a1;
+	v2 = *a1;
+	for ( result = -1; v2; ++v1 )
+	{
+		result = v2 + 33 * result;
+		v2 = v1[1];
+	}
+	return result;
+}
+
+bool __fastcall UnlockSystem_IsDLCUnlock( void *_this, void *_edx, int unlock )
+{
+	return ( std::find( g_dlcList.begin(), g_dlcList.end(), unlock ) != g_dlcList.end() );
+}
+
+void Initialize()
+{
+	std::ifstream file( "dlc.txt" );
+	if ( !file.is_open() )
+		return;
+
+	std::string str;
+	while ( std::getline( file, str ) )
+	{
+		if ( str.empty() )
+			continue;
+
+		g_dlcList.push_back( bStringHash( str.c_str() ) );
+	}
+
+	if ( MH_Initialize() != MH_OK )
+		return;
+
+	if ( MH_CreateHook( (LPVOID)0x820280, &UnlockSystem_IsDLCUnlock, NULL ) != MH_OK )
+		return;
+
+	if ( MH_EnableHook( MH_ALL_HOOKS ) != MH_OK )
+		return;
+}
+
+BOOL WINAPI DllMain( HINSTANCE /*hinstDLL*/, DWORD fdwReason, LPVOID /*lpvReserved*/ )
+{
+	switch ( fdwReason )
+	{
+		case DLL_PROCESS_ATTACH:
+		case DLL_THREAD_ATTACH:
+		case DLL_PROCESS_DETACH:
+		case DLL_THREAD_DETACH:
+		default:
+			break;
+	}
+	return TRUE;
+}
+
+extern "C" __declspec( dllexport ) void InitializeASI()
+{
+	Initialize();
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Need for Speed: Carbon DLC Unlocker
+
+This is a plugin that hooks Need for Speed: Carbon DLC system, allowing you to unlock DLC content without modifying the game's assets or manually editing the save file. By default, all six DLC exclusive cars are unlocked (Porsche 911 Turbo, Dodge Challenger R/T, Toyota Corolla GT-S, Infiniti G35, Shelby GT500 '06, Pagani Zonda F).
+
+**Note that this plugin only works with a cracked v1.4 game executable. Older versions and uncracked executables with SafeDisc are not supported.**
+
+## Usage
+
+1. Extract all files from the [latest release](https://github.com/NicknineTheEagle/NFSCarbonDLCUnlocker/releases/latest) into the game directory.
+
+2. (OPTIONAL) Edit scripts\dlc.txt and add the internal names of any unlocks you wish to obtain. You can find known DLC unlock lists in dlc_examples directory.
+
+3. Play the game. DLC content should become available immediately, you do not need to create a new save file.
